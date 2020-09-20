c0077847362b124b0c84e50408607e4bf08e2c47
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-20 10:23:28 -0400

    header done

---
+++ slack-clone/src/Firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCq2K7tmV7KEJj18SU1hbwDlu4nSDqhyFs",
+  authDomain: "slack-clone-8dc37.firebaseapp.com",
+  databaseURL: "https://slack-clone-8dc37.firebaseio.com",
+  projectId: "slack-clone-8dc37",
+  storageBucket: "slack-clone-8dc37.appspot.com",
+  messagingSenderId: "597981284039",
+  appId: "1:597981284039:web:5fe4779ff59f7f8bcca0e2",
+  measurementId: "G-CHCXZ86FCK",
+};
--- slack-clone/src/Header.js
+++ slack-clone/src/Header.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./Header.css";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 
 function Header() {
   return (
     <div className="header">
-      <div className="header__left"></div>
-      <div className="header__search"></div>
-      <div className="header__right"></div>
-      Header
+      <div className="header__left">
+        <Avatar className="header__avatar" alt="tiff" />
+        <AccessTimeIcon />
+      </div>
+      <div className="header__search">
+        <SearchIcon />
+        <input placeholder="Search here..." />
+      </div>
+      <div className="header__right">
+        <HelpOutlineIcon />
+      </div>
     </div>
   );
 }
