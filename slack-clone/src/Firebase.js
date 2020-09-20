17cd4940823010767a556b1b07fc668f0478d5c9
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-20 13:02:09 -0400

    setting the firebase database

--- slack-clone/src/Firebase.js
+++ slack-clone/src/Firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCq2K7tmV7KEJj18SU1hbwDlu4nSDqhyFs",
@@ -9,3 +11,11 @@ const firebaseConfig = {
   appId: "1:597981284039:web:5fe4779ff59f7f8bcca0e2",
   measurementId: "G-CHCXZ86FCK",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
--- slack-clone/src/Sidebar.js
+++ slack-clone/src/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Sidebar.css";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
@@ -13,8 +13,22 @@ import FileCopyIcon from "@material-ui/icons/FileCopy";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 import AddIcon from "@material-ui/icons/Add";
+import db from "./firebase";
 
 function Sidebar() {
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => {
+    db.collection("rooms").onSnapshot((snapshot) =>
+      setChannels(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name,
+        }))
+      )
+    );
+  }, []);
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -40,6 +54,10 @@ function Sidebar() {
       <SidebarOption Icon={ExpandLessIcon} title="Channels" />
       <hr />
       <SidebarOption Icon={AddIcon} title="Add channel" />
+
+      {channels.map((channel) => (
+        <SidebarOption title={channel.name} id={channel.id} />
+      ))}
     </div>
   );
 }
