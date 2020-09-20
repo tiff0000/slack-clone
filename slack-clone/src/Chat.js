cfa3bbdaaf1026c8fe0e9ef483036f76551a30d6
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-20 14:38:58 -0400

    chat title synchronized with sidebar

--- slack-clone/src/Chat.js
+++ slack-clone/src/Chat.js
@@ -3,15 +3,27 @@ import "./Chat.css";
 import { useParams } from "react-router-dom";
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import db from "./firebase";
+import { useState, useEffect } from "react";
+
 function Chat() {
   const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = useState(null);
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomDetails(snapshot.data()));
+    }
+  }, [roomId]);
+
   return (
     <div className="chat">
-      <h2>Welcome to {roomId} room </h2>
       <div className="chat__header">
         <div className="chat__headerLeft">
           <h4 className="chat__channelName">
-            <strong># general</strong>
+            <strong># {roomDetails?.name}</strong>
             <StarBorderOutlinedIcon />
           </h4>
         </div>
