60624fa06e38bccac07ab4e0c722be2f93a5e777
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-20 09:11:22 -0400

    initiated slack react app

---
+++ slack-clone/src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left"></div>
+      <div className="header__search"></div>
+      <div className="header__right"></div>
+      Header
+    </div>
+  );
+}
+
+export default Header;
